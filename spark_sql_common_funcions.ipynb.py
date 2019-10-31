6016254ec252ccd93b235593b65a397ccc6960da
cbgaete <cbgaete@entel.cl>
2019-10-31 18:10:16 -0300

    lambda rating and movie

---
+++ spark_sql_common_funcions.ipynb.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SQLContext, Row
+from pyspark.sql.functions import asc, desc
+
+sqlContext = SQLContext(sc)
+
+movieText= sc.textFile('/user/cloudera/movies.dat')
+ratingText= sc.textFile('/user/cloudera/ratings.dat')
+
+ratingsData = ratingText.map(lambda line: line.split(':'))
+movieData = movieText.map(lambda line: line.split(':'))
+
